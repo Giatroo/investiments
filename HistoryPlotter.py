962d05dfb45e482429b73d16a2a403b42c3a2c5b
Lucas Paiolla <lucaspaiolla@gmail.com>
2022-02-17 18:27:42 -0300

    change HistoryPlotter state
    HistoryPlotter now doesn't keep a single ticker to print. Each method
    can have different parameters.
    
    add multi_lineplot method to plot more than one stocks

--- HistoryPlotter.py
+++ HistoryPlotter.py
@@ -1,76 +1,77 @@
 from abc import ABC, abstractmethod
-from matplotlib import ticker
 
 from pandas import DataFrame
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from cycler import cycler
 
 from HistoryInformationRetriever import HistoryInformationRetriever
 
 
 class HistoryPlotter(ABC):
-    _info_retriever: DataFrame
-
-    def __init__(
+    @abstractmethod
+    def lineplot(
         self,
-        ticker_code: DataFrame,
+        ticker_code: str,
         **history_kwargs
     ) -> None:
-        self._info_retriever = HistoryInformationRetriever(ticker_code, **history_kwargs)
-
-    @abstractmethod
-    def lineplot(self) -> None:
         pass
 
     @abstractmethod
-    def candlestick(self) -> None:
+    def multi_lineplot(
+        self,
+        history_df: DataFrame
+    ) -> None:
         pass
 
     @abstractmethod
-    def dividends(self) -> None:
+    def candlestick(
+        self,
+        ticker_code: str,
+        **history_kwargs
+    ) -> None:
+        pass
+
+    @abstractmethod
+    def dividends(
+        self,
+        ticker_code: str,
+        **history_kwargs
+    ) -> None:
         pass
 
 
 class MatplotHistoryPlotter(HistoryPlotter):
-    def __init__(self,
-                 ticker_code: DataFrame,
-                 theme: str = 'onedark',
-                 **history_kwargs
-                 ) -> None:
-        super().__init__(ticker_code, **history_kwargs)
+    def __init__(
+        self
+    ) -> None:
+        plt.rcdefaults()
+        plt.rcParams['axes.prop_cycle'] = cycler(color='bgrcmyk')
 
-        if theme == 'onedark':
-            self._set_onedark_theme()
+    def lineplot(
+        self,
+        ticker_code: str,
+        ax = None,
+        **history_kwargs
+    ) -> None:
+        info_retriever = HistoryInformationRetriever(ticker_code, **history_kwargs)
 
-    def _set_onedark_theme(self):
-        self._background_color = '#292C34'
-        self._green_color = '#98C379'
-        self._red_color = '#E06C75'
-        self._white_color = '#ABB2BF'
+        variation = info_retriever.get_variation()
+        series = info_retriever.get_values_over_time()
+        time_interval = info_retriever.get_interval()
+        ticker_code = info_retriever._ticker_code
 
-        plt.rcParams.update({'text.color': self._white_color,
-                            'xtick.color': self._white_color,
-                             'ytick.color': self._white_color,
-                             'axes.facecolor': self._background_color,
-                             'figure.facecolor': self._background_color,
-                             'lines.color': self._white_color
-                             })
+        color = 'C1' if variation > 0 else 'C2'
 
-    def lineplot(self) -> None:
-        variation = self._info_retriever.get_variation()
-        series = self._info_retriever.get_values_over_time()
-        time_interval = self._info_retriever.get_interval()
-        ticker_code = self._info_retriever._ticker_code
-
-        color = self._green_color if variation > 0 else self._red_color
-
-        fig, ax = plt.subplots(1, 1, figsize=(15, 3))
-        plt.plot(series.index, series, linewidth=5, c=color)
+        if ax is None:
+            _, ax = plt.subplots(1, 1, figsize=(15, 3))
+        plt.plot(series.index, series, linewidth=3, c=color)
 
         ax.set_xlim(time_interval)
         ax.set_ylabel('')
         ax.set_xlabel('')
 
-        ax.set_title(f'{ticker_code} ({variation * 100:.2f}%)', 
+        ax.set_title(f'{ticker_code} ({variation * 100:.2f}%)',
                      color=color,
                      fontsize='x-large',
                      fontweight='bold')
@@ -79,21 +80,50 @@ class MatplotHistoryPlotter(HistoryPlotter):
         ax.tick_params(bottom=False, left=False)
         ax.grid(which='major', axis='y', linewidth=0.1)
 
-        fig.show()
+    def multi_lineplot(
+        self,
+        history_df : DataFrame,
+        ax = None
+    ) -> None:
+        if ax is None:
+            _, ax = plt.subplots(1, 1, figsize=(15, 3))
 
-    def candlestick(self) -> None:
+        for ticker_code in history_df.columns:
+            plt.plot(history_df.index, history_df[ticker_code], linewidth=1, label=ticker_code)
+
+        time_interval = history_df.index.min(), history_df.index.max()
+        ax.set_xlim(time_interval)
+        ax.set_ylabel('')
+        ax.set_xlabel('')
+
+        ax.spines[:].set_visible(False)
+        ax.tick_params(bottom=False, left=False)
+        ax.grid(which='major', axis='y', linewidth=0.1)
+
+        ax.legend()
+
+    def candlestick(
+        self,
+        ticker_code: str,
+        **history_kwargs
+    ) -> None:
         return super().candlestick()
 
-    def dividends(self) -> None:
-        series = self._info_retriever.get_dividends_over_time()
-        ticker_code = self._info_retriever._ticker_code
+    def dividends(
+        self,
+        ticker_code: str,
+        ax = None,
+        **history_kwargs
+    ) -> None:
+        info_retriever = HistoryInformationRetriever(ticker_code, **history_kwargs)
+        series = info_retriever.get_dividends_over_time()
 
+        if ax is None:
+            _, ax = plt.subplots(1, 1, figsize=(15, 3))
+        ax.bar(series.index, series, width=5, color='C1')
 
-        fig, ax = plt.subplots(1, 1, figsize=(15, 3))
-        ax.bar(series.index, series, width=5, color=self._green_color)
-
-        ax.set_title(f'{ticker_code} dividends', 
-                     color=self._green_color,
+        ax.set_title(f'{ticker_code} dividends',
+                     color='C1',
                      fontsize='x-large',
                      fontweight='bold')
 
