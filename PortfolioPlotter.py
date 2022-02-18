22dc5a226595947ad742611861db29b00572c39e
Lucas Paiolla <lucaspaiolla@gmail.com>
2022-02-17 21:11:08 -0300

    change how the portfolio is printed

--- PortfolioPlotter.py
+++ PortfolioPlotter.py
@@ -1,3 +1,4 @@
+from statistics import variance
 from cv2 import norm
 from matplotlib.pyplot import hist
 import pandas as pd
@@ -32,17 +33,34 @@ class PortfolioPlotter():
         normalized_history_df = self._portfolio.get_normalized_history_df(**history_kwargs)
         self._plotter.multi_lineplot(normalized_history_df)
 
+    def plot_portfolio_as_stock(
+        self,
+        **history_kwargs
+    ) -> None:
+        portfolio_values = self._portfolio.get_portfolio_values(**history_kwargs)
+        
+        first_vale = portfolio_values.iloc[0]
+        last_value = portfolio_values.iloc[-1]
+        variation = (last_value - first_vale) / first_vale
+
+        color = 'g' if variation > 0 else 'r'
+        title = f'Portfolio ({variation * 100:.2f}%)'
+
+        self._plotter.multi_lineplot(pd.DataFrame(portfolio_values),
+                                     colors=[color],
+                                     title=title)
+
     def plot_portfolio(
         self,
         **history_kwargs
     ) -> None:
-        portfolio_df = self._portfolio.get_portfolio_df(**history_kwargs)
-        # temporary
-        self._plotter.multi_lineplot(pd.DataFrame(portfolio_df['portfolio']))
-
-    def plot_portfolio_and_individual_tickers(
-        self,
-        **history_kwargs
-    ) -> None:
         portfolio_df = self._portfolio.get_normalized_portfolio_df(**history_kwargs)
-        self._plotter.multi_lineplot(portfolio_df)
+
+        num_stocks = len(portfolio_df.columns) - 1
+        linewidths = [1] * num_stocks + [3]
+        linestyles = ['--'] * num_stocks + ['-']
+
+        self._plotter.multi_lineplot(portfolio_df, 
+                                     linewidths=linewidths,
+                                     linestyles=linestyles,
+                                     title='Portfolio performance')
